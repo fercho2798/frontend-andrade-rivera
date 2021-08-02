65b3063815ce6f481161af49e2bab334aaccf9dc
Fernando Rivera <ferchofercho933@gmail.com>
2021-08-02 14:56:51 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthorComponent } from './author/author.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MainComponent
+  },
+  {
+    path:'projects',
+    component:ProjectComponent
+  },
+  {
+    path:'authors',
+    component:AuthorComponent
+  },
+  {
+    path: 'notFount', component: NotFoundComponent
+  },
+   {
+    path: '**', redirectTo: 'notFount'
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
+
+import { ProjectComponent } from './project/project.component';
+import { AuthorComponent } from './author/author.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MainComponent } from './main/main.component';
+import { AccesDeniedComponent } from './acces-denied/acces-denied.component';
+import { UnderMaintenanceComponent } from './under-maintenance/under-maintenance.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    AuthorComponent,
+    NotFoundComponent,
+    MainComponent,
+    AccesDeniedComponent,
+    UnderMaintenanceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputSwitchModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
