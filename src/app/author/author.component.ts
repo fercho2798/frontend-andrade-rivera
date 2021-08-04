1167aac8de3887d051321bbd4b0a4dc18682aa33
Fernando Rivera <ferchofercho933@gmail.com>
2021-08-04 18:45:33 -0500

    changes in the authors

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthorModel } from '../models/author.model';
 import { AuthorHttpService } from '../services/author-http.service';
 
@@ -9,11 +10,11 @@ import { AuthorHttpService } from '../services/author-http.service';
 })
 export class AuthorComponent implements OnInit {
 
-  author: AuthorModel = {};
+  selectedAuthor: AuthorModel = {};
   authors:AuthorModel[]=[];
-
-  constructor(private authorHttpService:AuthorHttpService) {
-
+  formAuthor: FormGroup;
+  constructor(private authorHttpService:AuthorHttpService, private formBuilder: FormBuilder) {
+    this.formAuthor = this.newFormAuthor();
 
    }
  
@@ -22,66 +23,116 @@ export class AuthorComponent implements OnInit {
   this. getAuthor();
   }
 
-  getAuthors():void{
+  newFormAuthor(): FormGroup {
+    return this.formBuilder.group(
+      {
+        id: [null],
+        project: [null],
+        names: [null, ],
+        email: [null],
+        phone: [null],
+        identification: [null],
+        age:[null]
+
+
+      }
+    )
+  }
+
+
+
+
+  getAuthors(): void {
+
     this.authorHttpService.getAll().subscribe(
-      response => {
-        console.log(response);
-         this.author = response['data'];
 
+      response => {
+
+        this.authors = response['data'] as AuthorModel[];
       },
-      error=>{
-        console.log(error);
+
+      error => {
+        console.log(error)
       }
-    )
+    );
   }
-  getAuthor():void{
-    this.authorHttpService.getOne(this.author.id).subscribe(
-      response => {
-        console.log(response);
-         this.author = response['data'];
+  getAuthor(): void {
+    const id = 1;
+    this.authorHttpService.getOne(id).subscribe(
 
+      response => {
+
+        this.selectedAuthor = response['data'];
       },
-      error=>{
-        console.log(error);
+
+      error => {
+        console.log(error)
       }
-    )
+    );
   }
-  postAuthors():void{
-    this.authorHttpService.create(this.author).subscribe(
-      response => {
-        console.log(response);
-         this.author = response['data'];
+  
+  createAuthor(): void {
 
-      },
-      error=>{
-        console.log(error);
-      }
-    )
-  }
-  updateAuthors():void{
-    this.authorHttpService.update(this.author.id, this.author).subscribe(
-      response => {
-        console.log(response);
-         this.author = response['data'];
+    this.authorHttpService.create(this.selectedAuthor).subscribe(
 
-      },
-      error=>{
-        console.log(error);
-      }
-    )
-  }
-  deleteAuthors():void{
-    this.authorHttpService.delete(this.author.id).subscribe(
       response => {
-        console.log(response);
-         this.author = response['data'];
-
+        console.log(response)
       },
-      error=>{
-        console.log(error);
+
+      error => {
+        console.log(error)
       }
-    )
+    );
   }
 
+  updateAuthor(author: AuthorModel): void {
+
+    this.authorHttpService.update(author.id, author).subscribe(
+
+      response => {
+        console.log(response)
+      },
+
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  deleteAuthor(author: AuthorModel): void {
+
+    this.authorHttpService.delete(author.id).subscribe(
+
+      response => {
+        console.log(response)
+        this.removeAuthor(author);
+      },
+
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  removeAuthor(author: AuthorModel){
+    this.authors = this.authors.filter(element => element.id !== author.id);
+  }
+
+
+  selectAuthor(author: AuthorModel) {
+    console.log(author)
+    this.formAuthor.patchValue(author)
+  }
+
+  onSubmit(){
+    console.log('subido!')
+  }
+
+  get idField(){
+    return this.formAuthor.controls['id']
+  }
+  get codeField(){
+    return this.formAuthor.controls['code']
+  }
 }
 
